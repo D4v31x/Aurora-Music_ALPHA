5b7b290d87217bfd9dc7c354534566399477bc33
David84 <davidbrichacek28@gmail.com>
2024-05-27 18:27:07 +0200

    Cleanup in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:aurora_music_v01/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final User? _currentUser = _auth.currentUser;
-
-  
 
   runApp(FutureBuilder(
     future: Firebase.initializeApp(),
